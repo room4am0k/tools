7ffb60a48ec0a44eb78764ac41074c7a9f4472e2
Diego <75104746+room4am0k@users.noreply.github.com>
2025-09-14 09:33:32 -0300

    Create db.go
    postgre database connection module

---
+++ db/db.go
@@ -0,0 +1,48 @@
+package db
+
+// db module connection.
+// uses env file to access keys
+// has fallbacks credentials so be careful.
+// use db.Init to start the connection.
+// use db.DB.Query("") to make sql queries, store in rows, err.
+// defer -> rows.Close()
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	
+	_ "github.com/jackc/pgx/v5/stdlib" // pgx driver database/sql
+)
+
+var DB *sql.DB
+
+func Init() {
+	user := getEnv("PGUSER","postgres")
+	pass := getEnv("PGPASSWORD","postgres")
+	host := getEnv("PGHOST","localhost")
+	port := getEnv("PGPORT","5432")
+	dbname := getEnv("PGDATABASE","postgres")
+	
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",user,pass,host,port,dbname)
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+	DB.SetConnMaxLifetime(30*time.Minute)
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := DB.PingContext(ctx); err != nil{
+		log.Fatalf("[1] Not connected: %v", err)
+	}
+	log.Println("Connected to DB")
+}
+
+func getEnv(key, fallback string) string {
+	if vak := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
